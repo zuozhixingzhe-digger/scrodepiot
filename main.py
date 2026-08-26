3f29edab663902f636b45b0cb6a05051a33d7261
zuozhixingzhe-digger <321323006+zuozhixingzhe-digger@users.noreply.github.com>
2026-08-26 19:35:04 +0800

    default-message:zuozhixingzhe-digger;作止行者;This info is created on 20260826

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# --digger-created--20260725
+# zuozhixingzhe/digger/作止行者 (c) all rights reserved;
+# Contact me at 321323006+zuozhixingzhe-digger@users.noreply.github.com
+# scrodepiot:main.py
+# this is just the main process -- all the contents are here(except when we make it too huge to make it in a file )
+
+
+
+def core():
+    print('hello')
+
+def main():
+    core()
+
+
+
+
+if __name__ == '__main__':
+    main()
