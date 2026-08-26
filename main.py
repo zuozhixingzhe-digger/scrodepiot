2e7d8fe553d7e3484185d4d9e1de1a687e4078bb
zuozhixingzhe-digger <321323006+zuozhixingzhe-digger@users.noreply.github.com>
2026-08-26 20:46:56 +0800

    Just a start

--- main.py
+++ main.py
@@ -1,20 +1,74 @@
 #!/usr/bin/env python3
 # --digger-created--20260725
-# zuozhixingzhe/digger/作止行者 (c) all rights reserved;
-# Contact me at 321323006+zuozhixingzhe-digger@users.noreply.github.com
+#  zuozhixingzhe/digger/作止行者 (c) all rights reserved;
+# Contact me at `321323006+zuozhixingzhe-digger@users.noreply.github.com`
 # scrodepiot:main.py
 # this is just the main process -- all the contents are here(except when we make it too huge to make it in a file )
+# --!!!--GIT ENABLED --DO NOT put any sensitive (e.g. key) or non-plaintext (e.g. big photos) or excessive content(e.g. libraries) files here,even in an instant
+# I-m a green hand,and this is just a play and clown project,do not use it in your production environment.
+# If you are interested in who I am ,I-ll tell you that I am a Chinese.
+# --!!!--the LICENCE has not been choosed, but probably `Apache 2.0 or MIT` license, for you to choose. If you see this line, please remind me to add the licence, thanks!
+# only the that just compatible with both Apache 2.0 and MIT are allowed to use.
+# e.g. PSFv2 MIT Public-Domain BSD-3-Clause-License,..., are allowed.
+# e.g. GPLv2 GPLv3 LGPL AGPL Apache 2.0 ,..., these libraries and source codes are NOT allowed to use.
+
+"""
+```markdown
+# DESIGN
+### PINNED: refences, open source usage disclaiminer and acknowledgements
+- ???[MUST add this!!!(:/licences)]
+- licenses
+    - .
+
+
+### TODO LIST
+- add the licenses of the library I have used.(IMPORTANT)
+
+### dependences
+
+### licence
+- ???
+
+###
+- chain:
+    - core (if-name-=-main -> router -> main -> core):
+        -
 
 
 
-def core():
-    print('hello')
+
+```
+"""
+
+################### BEGIN -ING of the code ###############################
+import click # BSD 3-Clause License
+
+
+class CoreProcesses:
+    def core():
+
+        """
+        ### safety check
+        """
+
+        """
+        ### read parameters
+        """
+
+
+        print('hello')
+
 
 def main():
-    core()
+    CoreProcesses.core()
 
-
+def router():
+    # You could modify your own test or other logic or idea here, which would not easily break the main process of my program.
+    #main()
+    # You could just use a single `main()` as default origin behavior.
+    main()
 
 
 if __name__ == '__main__':
-    main()
+    router()
+################### END of the code ###############################
