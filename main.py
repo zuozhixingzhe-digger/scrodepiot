a598f02baa0a0d65558dc104b4fba84603f59877
zuozhixingzhe-digger <321323006+zuozhixingzhe-digger@users.noreply.github.com>
2026-09-02 23:18:07 +0800

    Add Indent test

--- main.py
+++ main.py
@@ -57,21 +57,90 @@ PROGRAM_HELP_DOC= f"""
 # settings
 ENCODING = "utf-8" # do not delete;
 GRAMMAR = r"""
+    //
+    %import common.WS_INLINE
+    %import common.END_OF_FILE -> EOF
+    %declare _INDENT _DEDENT    // Virtual tags
+    //
     start: executable?            // beginning with a `?` makes the tree simple; `executable` is just a whole unit; only no or one `executable` is allowed.
-    ?executable: statement+
-    ?statement: debug
-    ?debug: SYMBOL|NUMERIC|EXPANDEDKEYWORD|REGEXMATCH|REGEXREPLACE|PLUS
-    PLUS: /\+/
-    MINUS: /\-/
-    MULTIPLY: /\*/
+    executable: statement+        // TODO: You could write ?excutable here(when the rules allows).
+    statement: ((expression ASSIGNCOMMAND expression) | (expression)) (_NL*)   // ;. WARNING TODO: statement =/= expression
+    debugthing: SYMBOL|NUMERIC|EXPANDEDKEYWORD|REGEXMATCHER|REGEXREPLACER|URL|TRUE|POWER|PLUS|MINUS|MULTIPLY|DEVIDE|COMMA|DOT|_INDENT|_DEDENT|_NL|string
+    expression: debugthing|bracketexpression|expression POWER expression
+    bracketexpression: "(" expression ")"
+        | "[" expression "]"
+        | "{" expression "}"  // That-s strange: the lark grammar, cannot be `xxx |` ,but `| xxx`.
+    //    the keyword is so many (and may increase in the future), that we use a `-` prefix to tell between symbols and keywords.
+    string: ESCAPEDSTRING | RAWSTRING
+    TRY: "-try"          // try-but(`except`)-normally(`else`)-always(`finally`)
+    BUT: "-but"
+    NORMALLY: "-normally"
+    ALWAYS: "-always"
+    SHOCK: "-shock"  // (`raise`)You must catch a `shock`, otherwise, the program will terminate.
+    SHOUT: "-shout"   // Special! You could choose not to solve `shout`, the program will continue; if you use `but` to catch it, then, if you already solve this, then you could choose `-next`(come back to the original position, default, nothing would happen, and `normally` is to be executed) or `-leave`(do not execute the `normally`, and `always` is the last what we do, before leave the block.).
+    DISCUSS: "-discuss"   // discuss-case (is like C-s switch-case);
+    CASE: "-case"
+    IF: "-if"   // You could get rid of `if`, `<condition><return><indented block>` is a condition(if) statement; like Python, there is `x -if y -else z`.
+    ELSE: "-else"    // `-else` or `-else <condition>`(`elif <condition>:`)
+    LOOP: "-loop"       // `<condition> -loop` or `-loop`(`while True:`)
+    ITERATE: "-iterate"    // (`for`), `-iterate x-iterable -as y-new-var -when condition`.
+    NEXT: "-next"  // (`continue`)
+    LEAVE: "-leave"   // (`break`)   next and leave, just as the queue:).
+    PROCESSTYPE: "-process"
+    BOOLEANTYPE: "-boolean"
+    INTEGERTYPE: "-integer"
+    REALTYPE: "-real"
+    COMPLEXTYPE: "-complex"
+    MAPTYPE: "-map"    // (`dictionary` and `object`)
+    VECTORTYPE: "-vector"
+    STRINGTYPE: "-string"
+    NULL: "-null" | "-Null" | "-none" | "-None"
+    TRUE: "-true" | "-True" | "-t" | "-yes" | "-y" | "-on" | "-v" | "-V"   // Victory(v)/Cross(x) is allowed
+    FALSE: "-false" | "-False" | "-f" | "-no" | "-n" | "-off" | "-x" | "-X"
+    MOD: "-mod"
+    SIN: "-sin"
+    COS: "-cos"
+    TAN: "-tan"
+    // No `f"..."` as Python, we use `"..." expression "..."` instead; `="..."` is a raw String;
+    RAWSTRING: /=`[^`]*`/
+        | /='[^']*'/
+        | /="[^"]*"/
+        | /=```[\s\S]*```/
+        | /='''[\s\S]*'''/
+"""  r'''
+        | /="""[\s\S]*"""/
+'''  r"""
+    ESCAPEDSTRING: /`(?:[^\\]|\\[^\n])*[`$]/
+        | /'(?:[^\\]|\\[^\n])*['$]/
+        | /"(?:[^\\]|\\[^\n])*["$]/
+        | /```(?:[^\\]|\\[^\n])*```/
+        | /'''(?:[^\\]|\\[^\n])*'''/
+"""  r'''
+        | /"""(?:[^\\]|\\[^\n])*"""/
+'''  r"""
     SYMBOL: /[A-Za-z_][A-Za-z0-9_\-]*/
-    NUMERIC: /[0-9\-\+\.][A-Za-z0-9_\-\+]*.?/      //maybe we should enhance it with `1e-3` `0.1` `.1` etc.
+    NUMERIC: /[0-9\-\+]\.?[A-Za-z0-9_\-\+]*\.?/      //maybe we should enhance it with `1e-3` `0.1` `.1`, `0date-2026-09-02` etc.
     EXPANDEDKEYWORD: /-[A-Za-z0-9_\-]+/      // a single `-` is not included; the keywords is the additional, not the primary;
-    REGEXMATCH:  /\/(?:[^\/\\]|\\.)*\/[gimuy]*/             // to avoid colission with `/**/` & `//`, we need to take `*` away from the next from `/`(regex does not allow `*` to start, according to my knowledge(?)); Just tell the boarder of regex, do not check its grammar; `(?:)` does not catch the group in regex; Deepseek wrote it;
-    REGEXREPLACE: /\/.*\/.*\//   // ???????????
-    DEVIDE: /\//
-    COMMENT: /\/\/.*/ | /\/\*[\s\S]*\*\// | /#.*$/              // just bang(`#`)-comment, c-style (`//` , `/**/`); (`x'''...'''`is not included)
+    URL: /\<?(?:https?|file|s?ftp):\/\/[^\(\)\[\]\{\}<>`"\|\s]*\>?/
+        | /\<(?:https?|file|s?ftp):\/\/.*\>/
+        // Special! Directly write your URL! You could write directly or put into `<>`, such as `<https://www.example.com>` (which would write your words more freely, such as `()[]...`).(No space(use `%20` instead), \n, tab, `()[]{}|\ <>"`, ..., are allowed; use `%` to escape instead). .
+    REGEXMATCHER:  /\/(?:[^\/\\]|\\.)*\/[gimuy]*/             // to avoid colission with `/**/` & `//`, we need to take `*` away from the next from `/`(regex does not allow `*` to start, according to my knowledge(?)); Just tell the boarder of regex, do not check its grammar; `(?:)` does not catch the group in regex; Deepseek wrote it; regular expression matcher;
+    REGEXREPLACER: /\/.*\/.*\//   // ??????????? //replacer
+    PLUS: "+"
+    MINUS: "-"
+    MULTIPLY: "*"
+    DEVIDE: "/"
+    POWER: "**" | "^"
+    DOT: "."
+    COMMA: ","
+    NEXTSTATEMENT: ";"      // `;` Force to make it to the next statement
+    ASSIGNCOMMAND: "="    // assign-command(`=`) and assign-operator(`:=`) is different.
+    COMMENT: /\/\/.*/ | /\/\*[\s\S]*\*\// | "!" | "?" | /-[a-qs-z]*[a-z]*/ | /#.*$/               // just bang(`#`)-comment, c-style (`//` , `/**/`), and single sign-comment-char(Special! A single `!` or `?` is a comment); (`x'''...'''`is not included(TODO: THIS could add directly!!!));
     %ignore COMMENT
+    %ignore WS_INLINE
+    CONTINUED_LINE: /\\\r?\n[\x20\t]*/               //   `\<next-line>` to ignore the new-line tag. Just eat the `\n` and the space and tab(space and tab are not resolved by WS_INLINE).
+    %ignore CONTINUED_LINE
+    _NL: /\r?\n[\x20]*/           // the new line without which would be treated as the next-statement token. Only space(`\x20`) is legal now.
     //
     //
     //
@@ -104,9 +173,9 @@ try:
         UnexpectedEOF,
         VisitError,
         GrammarError,
-        LarkError  # 所有异常的父类
+        LarkError  # 所有异常的父类--deepseekAI
     ) # --deepseekAI
-    from lark.indenter import Indenter
+    from lark.indenter import Indenter  # Python-like Indention rules
     #
     import httpx # HACK LICENSE???
     #import NoThisModule
@@ -131,8 +200,21 @@ class CoreProcesses:
     """
     ### % Static Varibles
     """
-    scriptContent = None # This may be VERY big, so solve it with caution, do not duplicate or do other things.CoreProcesses.scriptContent
+    scriptContent = None # This variable may be VERY big, so solve it with caution, do not duplicate or do other things.CoreProcesses.scriptContent
     parser = None
+
+    """
+    ###
+    Indent solver
+    """
+    class UnifiedIndenter(Indenter):  # （统一的Unified）
+        # TODO:（原则上不允许tab的，根本就不允许tab作为缩进）
+        NL_type = '_NL'  # New Line（NL），defined in LARK GRAMMAR
+        INDENT_type = '_INDENT'
+        DEDENT_type = '_DEDENT'
+        OPEN_PAREN_types = []   # TODO: 换行里的没有闭合的括号要处理哦,问问deepseek
+        CLOSE_PAREN_types = []
+        tab_len = 4
     """
     ###
     """
@@ -169,7 +251,12 @@ class CoreProcesses:
         ### INIT
         """
         # Init lark
-        CoreProcesses.parser = Lark(GRAMMAR,parser = "lalr",)
+        CoreProcesses.parser = Lark(
+            GRAMMAR,
+            parser = "lalr",
+            regex = True,
+            postlex = CoreProcesses.UnifiedIndenter(),
+        )
         """
         ### %%% check parameters
             %%%% check script file and its arguments, route to interactive shell or a script reader(just execute).
@@ -255,7 +342,7 @@ class CoreProcesses:
 
     #
     @staticmethod
-    def interpreter(command:str): # Accept a `ExecutableBlock` once a time, which could be excuted independently.
+    def interpreter(command:str): # Accept a `ExecutableBlock` once a time, which could be excuted independently. Interprete it, and execute it.
         """
         ###
         .
@@ -321,3 +408,10 @@ def starterRouter():
 if __name__ == '__main__':
     starterRouter()
 ################### END of the code ###############################
+
+
+
+"""
+想法：
+- 一切可变，不要束缚于形式，比如引号，括号，缩进，关键字的可扩展性等
+"""
